1c88d48db55e0f91fc4ead247034ae3b51b5b951
Kyle Martin <kyle.martin.0819@gmail.com>
2023-10-27 16:56:46 -0400

    Revamped Main Page into Application Shell with Dashboard Layout
    Transformed the main page into the application's shell with a modern dashboard layout.
    Integrated the new navbar, sidebar, and the main dock for future ticket management, enhancing the overall user experience.

--- ticket-app/app/layout.js
+++ ticket-app/app/layout.js
@@ -1,5 +1,4 @@
 import { Inter } from 'next/font/google';
-import Nav from './(components)/Nav';
 import './globals.css';
 
 import { config } from '@fortawesome/fontawesome-svg-core';
@@ -17,14 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="flex flex-col h-screen max-h-screen">
-          <Nav />
-          <div className="flex-grow overflow-y-auto bg-white text-gray-800 dark:bg-gray-800 dark:text-white">
-            {children}
-          </div>
-        </div>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
