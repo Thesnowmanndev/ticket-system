9d7149c39b7ec2a0828df7c42b635d359557253a
Kyle Martin <kyle.martin.0819@gmail.com>
2023-10-29 18:08:00 -0400

    Added TicketCard, BtnDelete, PriorityRating, ProgressBar, StatusDisplay, and Dashboard components
    Created the TicketCard component, which includes sub-components: BtnDelete, PriorityRating, ProgressBar, and StatusDisplay.
    Added the Dashboard component to the project, facilitating ticket management and display.

---
+++ ticket-app/components/TicketCard/BtnDelete.jsx
@@ -0,0 +1,21 @@
+const BtnDelete = () => {
+  return (
+    <button
+      type="button"
+      className="inline-flex items-center mx-2 my-2 px-3 py-2 text-sm font-medium text-center rounded-lg bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+    >
+      <svg
+        className="w-6 h-6 text-white"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 18 20"
+      >
+        <path d="M17 4h-4V2a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v2H1a1 1 0 0 0 0 2h1v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1a1 1 0 1 0 0-2ZM7 2h4v2H7V2Zm1 14a1 1 0 1 1-2 0V8a1 1 0 0 1 2 0v8Zm4 0a1 1 0 0 1-2 0V8a1 1 0 0 1 2 0v8Z" />
+      </svg>
+      <span className="sr-only">Trash Bin</span>
+    </button>
+  );
+};
+
+export default BtnDelete;
