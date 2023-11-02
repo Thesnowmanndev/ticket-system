2f3c3b6f099b9815b82d5797aa8a2a3672fd544e
Kyle Martin <kyle.martin.0819@gmail.com>
2023-11-01 20:32:10 -0400

    feat: Implement ReadModal Component and Interaction
    - Created a new branch 'read-modal' and introduced a ReadModal component along with a dedicated 'components/ReadModal/' directory.
    - Added functionality to the dashboard to display the ReadModal component at the center of the screen with a blurred background when a user clicks on a ticket.
    - The ReadModal component currently displays static text and components from TicketCard.

--- ticket-app/components/Dashboard/Dashboard.jsx
+++ ticket-app/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,23 @@
+'use client';
+
+import { useState } from 'react';
+
 import Navbar from '@/components/Navbar/Navbar';
+import ReadModal from '@/components/ReadModal/ReadModal';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import TicketCard from '@/components/TicketCard/TicketCard';
 
 const Dashboard = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function openModal() {
+    setModalIsOpen(true);
+  }
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="antialiased bg-gray-50 dark:bg-gray-900 min-h-screen">
       <Navbar />
@@ -10,12 +25,31 @@ const Dashboard = () => {
 
       <main className="p-4 md:ml-64 pt-20 mt-8 lg:mt-0 ">
         <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 mb-4">
-          <TicketCard />
-          <TicketCard />
-          <TicketCard />
-          <TicketCard />
+          <div onClick={openModal}>
+            <TicketCard />
+          </div>
+          <div onClick={openModal}>
+            <TicketCard />
+          </div>
+          <div onClick={openModal}>
+            <TicketCard />
+          </div>
+          <div onClick={openModal}>
+            <TicketCard />
+          </div>
         </div>
       </main>
+
+      {modalIsOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="absolute inset-0 bg-gray-900 bg-opacity-50 backdrop-blur-sm">
+            {/* This creates the shaded and blurred background */}
+          </div>
+          <div className="relative">
+            <ReadModal onClick={closeModal} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
